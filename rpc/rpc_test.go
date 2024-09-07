9d62c9d3eda9fde389eaf5556ff35b6cb0ac232a
Eamon Burns (Laptop WSL) <me@eamonburns.com>
2024-09-07 09:27:57 -0700

    feat(rpc): add rpc module

---
+++ rpc/rpc_test.go
@@ -0,0 +1,20 @@
+package rpc_test
+
+import (
+	"testing"
+
+	"github.com/agent-e11/educationalsp/rpc"
+)
+
+type EncodingExample struct {
+	Testing bool
+}
+
+func TestEncode(t *testing.T) {
+	expected := "Content-Length: 16\r\n\r\n{\"Testing\":true}"
+	actual := rpc.EncodeMessage(EncodingExample{Testing: true})
+
+	if expected != actual {
+		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
